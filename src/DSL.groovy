3ebdfd474e5596caf5c2c637ef753c39f123273b
sandeep <sandeepaws2020@gmail.com>
2022-04-23 23:50:51 +0530

    Text

--- src/DSL.groovy
+++ src/DSL.groovy
@@ -1,7 +1,7 @@
 job('DSL-Tutorial-1-Test') {
     description("my first dsl script")
     scm {
-        git('https://github.com/Sandeepaws2020/Java_Sample.git',main)
+        git('https://github.com/Sandeepaws2020/Java_Sample.git',master)
     }
     triggers {
         scm('* * * * *')
